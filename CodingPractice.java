e88c4187a686436b30ff82dcbb42130d785fa8ad
Chris Carr and Cameron Jensen <chris@>
2017-03-13 09:34:39 -0700

    refactor askHowMuchCoding to return time to calculate total and print message based on total

--- CodingPractice.java
+++ CodingPractice.java
@@ -2,12 +2,24 @@ import java.io.Console;
 
 public class CodingPractice {
   public static void main(String[] args) {
-    askHowMuchCoding("Yesterday");
-    askHowMuchCoding("Two Days ago");
-    askHowMuchCoding("Today");
+    Integer total = new Integer(0);
+
+    total += askHowMuchCoding("Yesterday");
+    total += askHowMuchCoding("Two Days ago");
+    total += askHowMuchCoding("Today");
+
+    System.out.println("total: " + total);
+
+    if(total >= 6) {
+      System.out.println("Great job!");
+    } else if(total >= 3 && total < 6) {
+      System.out.println("Good job!");
+    } else {
+      System.out.println("Code more!");
+    }
   }
 
-  public static void askHowMuchCoding(String day) {
+  public static Integer askHowMuchCoding(String day) {
     Console console = System.console();
 
     System.out.println("How much time did you code " + day + "?");
@@ -15,5 +27,7 @@ public class CodingPractice {
 
     System.out.println("You spent " + intTime + " hours " + day);
     System.out.println("");
+
+    return intTime;
   }
 }
